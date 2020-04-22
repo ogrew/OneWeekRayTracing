79e72baf6c4e9e2b64a663face4d2a65255d7f64
ogrew <2044taiga@gmail.com>
2020-04-23 00:37:40 +0900

    DONE 第４章

--- main.cc
+++ main.cc
@@ -1,14 +1,26 @@
 #include <iostream>
 #include "ray.h"
 
+bool hit_sphere(const vec3& center, float radius, const ray& r) {
+    vec3 oc = r.origin() - center;                  // oc = A - C
+    float a = dot(r.direction(), r.direction());    // a = B・B
+    float b = 2.0 * dot(oc, r.direction());         // b = 2tB・(A - C)
+    float c = dot(oc, oc) - radius * radius;        // c = (A - C)・(A - C)-R^2
+    return (b * b - 4 * a * c) >= 0;
+}
+
 vec3 color(const ray& r) 
 {
+    // z=-1の位置に半径0.5の球体を置く
+    if(hit_sphere(vec3(0, 0, -1), 0.5, r)) {
+        return vec3(1, 0, 0);
+    }
+
     vec3 unit_direction = unit_vector(r.direction()); // -1.0 < y < 1.0
     float t = 0.5 * (unit_direction.y() + 1.0); // 0.0 < t < 1.0
 
     // t = 1.0 のとき青、t = 0.0のとき白(lerp = 線形補間)
     return (1.0 - t) * vec3(1.0,1.0,1.0) + t * vec3(0.5, 0.7, 1.0);
-    
 }
 
 int main()
