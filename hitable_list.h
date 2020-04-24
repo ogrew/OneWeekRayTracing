cfce20c5a8c7e1e41e1d4dbd7acce3ad8db99686
ogrew <2044taiga@gmail.com>
2020-04-25 00:38:10 +0900

    第５章まで

--- hitable_list.h
+++ hitable_list.h
@@ -17,16 +17,18 @@ class hitable_list: public hitable {
 
 bool hitable_list::hit(const ray& r, float tmin, float tmax, hit_record& rec) const {
     hit_record tmp_rec;
-    int hit_count;
+    bool hit_something = false;
     double closest_so_far = tmax;
     for (int i = 0; i < list_size; i++) {
+        // 先に手前の物体に衝突すると
+        // 後ろの物体にはclosest_so_farの範囲でhit判定に入らない
         if(list[i]->hit(r, tmin, closest_so_far, tmp_rec)) {
-            hit_count++;
+            hit_something = true;;
             closest_so_far = tmp_rec.t;
             rec = tmp_rec;
         }
     }
-    return (hit_count > 0);
+    return hit_something;
 }
 
 #endif
