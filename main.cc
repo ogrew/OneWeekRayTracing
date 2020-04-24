39ece5abbe58da18afe4abbd1b475919f513c144
ogrew <2044taiga@gmail.com>
2020-04-25 01:08:15 +0900

    第6章やった

--- main.cc
+++ main.cc
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <random>
 #include "sphere.h"
 #include "hitable_list.h"
 #include "float.h"
+#include "camera.h"
 
 vec3 color(const ray& r, hitable *world) 
 {
@@ -20,29 +22,34 @@ int main()
 {
     int nx = 200;
     int ny = 100;
+    int ns = 100;
     int max = 255;
 
     std::cout << "P3\n" << nx << " " << ny << "\n" << max << "\n";
-
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
 
     hitable *list[2];
     list[0] = new sphere(vec3(0, 0, -1), 0.5);
     list[1] = new sphere(vec3(0, -100.5, -1), 100);
     hitable *world = new hitable_list(list, 2);
 
+    camera cam;
+
+    std::random_device d;
+    std::uniform_real_distribution<double> rnd(0.0,1.0);
+
     for (int y = ny - 1; y >= 0; y--) {
         for (int x = 0; x < nx; x++) {
-            float u = float(x) / float(nx);
-            float v = float(y) / float(ny);
+            vec3 col(0, 0, 0);
+            for (int s = 0; s < ns; s++) {
+                float u = float(x + rnd(d)) / float(nx);
+                float v = float(y + rnd(d)) / float(ny);
 
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            
-            vec3 p = r.point_at_param(2.0);
-            vec3 col = color(r, world);
+                ray r = cam.get_ray(u, v);
+                vec3 p = r.point_at_param(2.0);
+                col += color(r, world);
+            }
+
+            col /= float(ns);
 
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
