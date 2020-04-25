d47eeafac4addfa072e53849e959db4d2f132b80
ogrew <2044taiga@gmail.com>
2020-04-25 16:00:31 +0900

    第9章までやった

--- main.cc
+++ main.cc
@@ -35,13 +35,14 @@ int main()
 
     std::cout << "P3\n" << nx << " " << ny << "\n" << max << "\n";
 
-    hitable *list[4];
-    list[0] = new sphere(vec3(0, 0, -1),        0.5, new lambertian(vec3(0.8, 0.5, 0.2)));
-    list[1] = new sphere(vec3(0, -100.5, -1),   100, new lambertian(vec3(0.4, 0.8, 0.1)));
-    list[2] = new sphere(vec3(1, 0, -1),        0.5, new metal(vec3(0.4, 0.6, 0.7), 1.0));
-    list[3] = new sphere(vec3(-1, 0, -1),       0.5, new metal(vec3(0.8, 0.8, 0.8), 0.2));
+    hitable *list[5];
+    list[0] = new sphere(vec3(0, 0, -1),        0.5, new lambertian(vec3(0.9, 0.3, 0.1)));
+    list[1] = new sphere(vec3(0, -100.5, -1),   100, new lambertian(vec3(0.3, 0.7, 0.6)));
+    list[2] = new sphere(vec3(1, 0, -1),        0.5, new metal(vec3(0.2, 0.7, 0.1), 0.8));
+    list[3] = new sphere(vec3(-1, 0, -1),       0.5, new dielectric(1.5));
+    list[4] = new sphere(vec3(-1, 0, -1),     -0.45, new dielectric(1.5));
     
-    hitable *world = new hitable_list(list, 4);
+    hitable *world = new hitable_list(list, 5);
 
     camera cam;
 
--- material.h
+++ material.h
@@ -5,7 +5,33 @@
 #include "hitable.h"
 #include "random.h"
 
-// v:入射ray, n:法線(単位ベクトル), 反射ray = v + 2 (v・n)*n
+float schlick(float cosine, float ref_idx) {
+    float r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1.0 - r0) * pow((1.0 - cosine), 5.0);
+}
+
+// attenuation -> 減衰
+// scattered -> 散乱
+class material {
+    public:
+        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const = 0;
+};
+
+// 【屈折】v:入射ray, n:法線(単位ベクトル), ni_over_nt:, refracted:
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted) {
+    vec3 unit_v = unit_vector(v);
+    float dt = dot(unit_v, n);
+    float discriminant = 1.0 - ni_over_nt * ni_over_nt * (1.0 - dt * dt);
+    if(discriminant > 0) {
+        refracted = ni_over_nt * (unit_v - n * dt) - n * sqrt(discriminant);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 【反射】 v:入射ray, n:法線(単位ベクトル), 反射ray = v + 2 (v・n)*n
 vec3 reflect(const vec3& v, const vec3& n) {
     return v - 2 * dot(v, n) * n;
 }
@@ -19,16 +45,50 @@ vec3 random_in_unit_sphere() {
     return p;
 }
 
-class material {
+class dielectric : public material {
     public:
-        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const = 0;
+        float ref_idx;
+        dielectric(float ri) : ref_idx(ri) {}
+        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+            vec3 outward_normal;
+            vec3 reflected = reflect(r_in.direction(), rec.normal);
+            float ni_over_nt;
+            attenuation = vec3(1.0, 1.0, 1.0);
+            vec3 refracted;
+
+            float reflect_prob;
+            float cosine;
+            
+            if(dot(r_in.direction(), rec.normal) > 0) {
+                outward_normal = -rec.normal;
+                ni_over_nt = ref_idx;
+                cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
+            } else {
+                outward_normal = rec.normal;
+                ni_over_nt = 1.0 / ref_idx;
+                cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+            }
+
+            if(refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
+                reflect_prob = schlick(cosine, ref_idx);
+            } else {
+                reflect_prob = 1.0;
+            }
+
+            if(random_double() < reflect_prob) {
+                scattered = ray(rec.p, reflected);
+            } else {
+                scattered = ray(rec.p, refracted);
+            }
+            return true;
+        }
 };
 
 class lambertian : public material {
     public:
         vec3 albedo;
         lambertian(const vec3& a) : albedo(a) {}
-        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const  {
+        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
              vec3 target = rec.p + rec.normal + random_in_unit_sphere();
              scattered = ray(rec.p, target-rec.p);
              attenuation = albedo;
